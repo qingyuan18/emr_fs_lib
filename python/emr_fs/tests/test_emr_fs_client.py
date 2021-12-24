7e987dafff0450e2b62631852e3e45f3bcc37788
qingyuan18 <qingyuan18@gmail.com>
2021-12-24 21:42:14 +0800

    commit code

--- python/emr_fs/tests/test_emr_fs_client.py
+++ python/emr_fs/tests/test_emr_fs_client.py
@@ -18,6 +18,7 @@ if __name__ == '__main__':
    features02 = {"customer_id":"string",	"age":"string","diabetes":"string","ejection_fraction":"string",	"high_blood_pressure":"string","platelets":"string","sex":"string","smoking":"string","DEATH_EVENT":"string","dt":"string"}
    emr_fg02 = client.create_feature_group("customer_advance","","customer_id","dt",features02)
    emr_fg02= emr_fs01.get_feature_group("customer_advance")
+   emr_fg02.print_info()
 
    #test feature group ingestion
    #source_feature_group_dataset = "s3://emrfssampledata/feature_store_introduction_custs.csv"
@@ -31,9 +32,9 @@ if __name__ == '__main__':
    #test feature group time travel query
    #emr_fg01.select_all().timeQuery("20211201000000","20211216000000").show(0)
    #test join
-   emr_fg01.select(["customer_id","city_code","state_code"]).join(emr_fg02.select_all(),"customer_id").show(5)
+   #emr_fg01.select(["customer_id","city_code","state_code"]).join(emr_fg02.select_all(),"customer_id").show(5)
    #test train dataset retrive
-   emr_fg02.create_training_dataset(name = "userProfile dataset",\
+   #emr_fg02.create_training_dataset(name = "userProfile dataset",\
                data_format = "tfrecord",\
                startDt="20211201000000",\
                endDt= "20211216000000",\
